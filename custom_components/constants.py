ee203ddc53edba76211a0dd106cace1e5426537a
tomlac40 <81432000+tomlac40@users.noreply.github.com>
2025-02-08 13:33:06 +0100

    Create constants.py

---
+++ custom_components/constants.py
@@ -0,0 +1,17 @@
+
+CONF_ADDRESS = "address"
+CONF_NAME = "name"
+CONF_TOKEN = "token"
+CONF_TOKEN_ADDRESS = "token_address"
+CONF_MAIN_COIN = "main_coin"
+CONF_EXPLORER_API_URL = "explorer_api_url"
+CONF_EXPLORER_API_KEY = "explorer_api_key"
+CONF_DECIMALS = "decimals"
+
+"""Used to fetch data from other blockchains which are not directly based on SIGNA, ..
+It is assumed that you want to fetch from an ETH fork by default."""
+CONF_BLOCKCHAIN = "blockchain"
+
+SUPPORTED_BLOCKCHAIN_SIGNUMORFORK = "SIGNUM_OR_FORK"
+SUPPORTED_BLOCKCHAIN_BTC = "BTC"
+SUPPORTED_BLOCKCHAINS = [SUPPORTED_BLOCKCHAIN_SIGNUMORFORK, SUPPORTED_BLOCKCHAIN_BTC]
